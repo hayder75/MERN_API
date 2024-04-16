43173c09ec31e518c34496dd9ff2124d1df2c50b
hayder75 <hayderaman75@gmail.com>
2024-04-16 21:49:59 +0300

    models and schemas

--- backend/routes/controllers/goalControllers.js
+++ backend/routes/controllers/goalControllers.js
@@ -1,10 +1,14 @@
-const aynchHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler')
+const Goal = require('../../model/goalModel')
+
+
 // get goals
 //route GET /apio/goals
 //access private
 
-const getGoal = aynchHandler(async(req,res) =>{
-    res.json({message:'Get goals'})
+const getGoal = asyncHandler(async(req,res) =>{
+    const goals = await Goal.find()
+    res.json(goals)
 }
 )
 
@@ -13,15 +17,17 @@ const getGoal = aynchHandler(async(req,res) =>{
 //route GET /apio/goals
 //access private
 
-const setGoal = aynchHandler(async (req,res)=>{
-    console.log(req.body);
+const setGoal = asyncHandler(async (req,res)=>{
     if(!req.body.text){
         res.status(400)
         throw new Error('please add a text fild')
     }
 
+    const goals = await Goal.create({
+        text: req.body.text
+    })
 
-    res.status(200).json({msg: 'Set Goals'})
+    res.status(200).json(goals)
 }
 
 )
@@ -30,8 +36,18 @@ const setGoal = aynchHandler(async (req,res)=>{
 //route GET /apio/goals
 //access private
 
-const updateGoal = aynchHandler(async (req,res)=>{
-    res.json({mes: `update goal ${req.params.id}`})
+const updateGoal = asyncHandler(async (req,res)=>{
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id , req.body , {
+        new: true,
+    })
+    res.json(updateGoal)
 }
 )
 
@@ -40,9 +56,18 @@ const updateGoal = aynchHandler(async (req,res)=>{
 //route GET /apio/goals
 //access private
 
-const deleteGoal =  aynchHandler(async(req,res)=>{
-    res.json({mes: `delete goal ${req.params.id}`})
+const deleteGoal =  asyncHandler(async(req,res)=>{
+    const goal = await Goal.findById(req.params.id)
 
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+  
+    await goal.remove() 
+
+    
+    res.json(req.params.id)
 }
 
 )
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+const connectDB = require('./consig/db')
 const PORT = process.env.PORT || 5000;
+
+connectDB()
 
 const app = express();
 
@@ -10,5 +16,6 @@ app.use(express.urlencoded({extended: false}))
 
 
 app.use('/api/goals',require('./routes/goalRoutes'));
+app.use(errorHandler)
 
 app.listen(PORT,()=> console.log('server started '));
